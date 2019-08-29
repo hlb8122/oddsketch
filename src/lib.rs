02a0c4cd3f8228e99500d403f088a96f3cf2ac39
Harry Barber <hlbarber93@gmail.com>
2019-08-29 14:16:18 +0700

    Use const generics to allow generic length

--- src/lib.rs
+++ src/lib.rs
@@ -1,17 +1,19 @@
-use core::ops::{BitXor, BitXorAssign, Deref, DerefMut};
+#![feature(const_generics)]
+
+use std::ops::{BitXor, BitXorAssign, Deref, DerefMut};
 
 #[derive(Clone)]
-pub struct Oddsketch(Vec<u8>);
+pub struct Oddsketch<const LEN: usize>([u8; LEN]);
 
-impl Oddsketch {
+impl<const LEN: usize> Oddsketch<{LEN}> {
     #[inline]
-    pub fn new(raw: Vec<u8>) -> Oddsketch {
+    pub fn new(raw: [u8; LEN]) -> Self {
         Oddsketch(raw)
     }
 
     #[inline]
     pub fn insert(&mut self, short_id: u64) {
-        let os_index = (short_id % (self.0.len() as u64 * 8)) as usize;
+        let os_index = (short_id % (LEN as u64 * 8)) as usize;
         self.0[os_index / 8] ^= 1 << (os_index % 8);
     }
 
@@ -39,38 +41,38 @@ impl Oddsketch {
 
     #[inline]
     pub fn size(&self) -> u32 {
-        let length = 8. * (self.0.len() as f64);
+        let LEN = 8. * (LEN as f64);
         let weight = f64::from(self.hamming_weight());
 
-        let size_approx = -length / 2. * f64::ln(1. - 2. * weight / length);
+        let size_approx = -LEN / 2.  * f64::ln(1. - 2. * weight / LEN);
 
         size_approx as u32
     }
 
     #[inline]
     pub fn size_alt(&self) -> u32 {
-        let length = 8. * (self.0.len() as f64);
+        let LEN = 8. * (LEN as f64);
         let weight = f64::from(self.hamming_weight());
 
-        let size_approx = f64::ln(1. - 2. * weight / length) / f64::ln(1. - 2. / length);
+        let size_approx = f64::ln(1. - 2. * weight / LEN) / f64::ln(1. - 2. / LEN);
 
         size_approx as u32
     }
 
     pub fn fold(self, size: usize) -> Vec<u8> {
-        let chunk_size = self.0.len() / size;
+        let chunk_size = LEN / size;
         self.chunks(chunk_size)
             .map(|chunk| chunk.iter().fold(0, |acc, x| acc ^ x))
             .collect()
     }
 }
 
-impl BitXor for Oddsketch {
+impl<const LEN: usize> BitXor for Oddsketch<{LEN}> {
     type Output = Self;
 
     #[inline]
     fn bitxor(self, rhs: Self) -> Self::Output {
-        let mut out = vec![0; self.0.len()];
+        let mut out = [0; std::mem::size_of::<Self>()];
         for (i, item) in out.iter_mut().enumerate() {
             *item = self.0[i] ^ rhs.0[i];
         }
@@ -79,7 +81,7 @@ impl BitXor for Oddsketch {
     }
 }
 
-impl BitXorAssign for Oddsketch {
+impl<const LEN: usize> BitXorAssign for Oddsketch<{LEN}> {
     #[inline]
     fn bitxor_assign(&mut self, rhs: Self) {
         for (i, item) in self.iter_mut().enumerate() {
@@ -88,17 +90,15 @@ impl BitXorAssign for Oddsketch {
     }
 }
 
-pub const DEFAULT_LEN: usize = 256;
-
-impl Default for Oddsketch {
+impl<const LEN: usize> Default for Oddsketch<{LEN}> {
     #[inline]
     fn default() -> Self {
-        Oddsketch(vec![0; DEFAULT_LEN])
+        Oddsketch([0; LEN])
     }
 }
 
-impl Deref for Oddsketch {
-    type Target = Vec<u8>;
+impl<const LEN: usize> Deref for Oddsketch<{LEN}> {
+    type Target = [u8; LEN];
 
     #[inline]
     fn deref(&self) -> &Self::Target {
@@ -106,7 +106,7 @@ impl Deref for Oddsketch {
     }
 }
 
-impl DerefMut for Oddsketch {
+impl<const LEN: usize> DerefMut for Oddsketch<{LEN}> {
     #[inline]
     fn deref_mut(&mut self) -> &mut Self::Target {
         &mut self.0
