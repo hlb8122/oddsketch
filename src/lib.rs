afb5d6e67e0a37917407eea0668a0b1dcadd9396
Harry Barber <harrybarber@protonmail.com>
2020-05-09 05:12:23 +0100

    Make length dynamic

--- src/lib.rs
+++ src/lib.rs
@@ -1,19 +1,17 @@
 use core::ops::{BitXor, BitXorAssign, Deref, DerefMut};
 
-pub const DEFAULT_LEN: usize = 256;
-
 #[derive(Clone)]
-pub struct Oddsketch([u8; DEFAULT_LEN]);
+pub struct Oddsketch(Vec<u8>);
 
 impl Oddsketch {
     #[inline]
-    pub fn new(raw: [u8; DEFAULT_LEN]) -> Oddsketch {
+    pub fn new(raw: Vec<u8>) -> Oddsketch {
         Oddsketch(raw)
     }
 
     #[inline]
     pub fn insert(&mut self, short_id: u64) {
-        let os_index = (short_id % (DEFAULT_LEN as u64 * 8)) as usize;
+        let os_index = (short_id % (self.0.len() as u64 * 8)) as usize;
         self.0[os_index / 8] ^= 1 << (os_index % 8);
     }
 
@@ -36,7 +34,7 @@ impl Oddsketch {
 
     #[inline]
     pub fn size(&self) -> u32 {
-        let length = 8. * (DEFAULT_LEN as f64);
+        let length = 8. * (self.0.len() as f64);
         let weight = f64::from(self.hamming_weight());
 
         let size_approx = -length / 2. * f64::ln(1. - 2. * weight / length);
@@ -46,7 +44,7 @@ impl Oddsketch {
 
     #[inline]
     pub fn size_alt(&self) -> u32 {
-        let length = 8. * (DEFAULT_LEN as f64);
+        let length = 8. * (self.0.len() as f64);
         let weight = f64::from(self.hamming_weight());
 
         let size_approx = f64::ln(1. - 2. * weight / length) / f64::ln(1. - 2. / length);
@@ -55,7 +53,7 @@ impl Oddsketch {
     }
 
     pub fn fold(self, size: usize) -> Vec<u8> {
-        let chunk_size = DEFAULT_LEN / size;
+        let chunk_size = self.0.len() / size;
         self.chunks(chunk_size)
             .map(|chunk| chunk.iter().fold(0, |acc, x| acc ^ x))
             .collect()
@@ -67,7 +65,7 @@ impl BitXor for Oddsketch {
 
     #[inline]
     fn bitxor(self, rhs: Self) -> Self::Output {
-        let mut out = [0; DEFAULT_LEN];
+        let mut out = vec![0; self.0.len()];
         for (i, item) in out.iter_mut().enumerate() {
             *item = self.0[i] ^ rhs.0[i];
         }
@@ -85,15 +83,17 @@ impl BitXorAssign for Oddsketch {
     }
 }
 
+pub const DEFAULT_LEN: usize = 256;
+
 impl Default for Oddsketch {
     #[inline]
     fn default() -> Self {
-        Oddsketch([0; DEFAULT_LEN])
+        Oddsketch(vec![0; DEFAULT_LEN])
     }
 }
 
 impl Deref for Oddsketch {
-    type Target = [u8; DEFAULT_LEN];
+    type Target = Vec<u8>;
 
     #[inline]
     fn deref(&self) -> &Self::Target {
